138250c525492c86f11f4573d6c3a08e5d60abfa
nachob99 <nachoarriola99@gmail.com>
2018-02-22 21:34:27 +0100

    Modificado toda la clase BallDemo por problema de exception, cambiado array normal de strings por un ArrayList

--- BallDemo.java
+++ BallDemo.java
@@ -1,24 +1,28 @@
 import java.awt.Color;
+import java.util.ArrayList;
 import java.util.Random;
+
 /**
  * Class BallDemo - a short demonstration showing animation with the 
  * Canvas class. 
  *
- * @author Michael KÃ¶lling and David J. Barnes
+ * @author Michael Kölling and David J. Barnes
  * @version 2011.07.31
  */
 
 public class BallDemo   
 {
     private Canvas myCanvas;
-    private BouncingBall[] misBolas;
+    private ArrayList<BouncingBall> misBolas;
+    private Random aleatorio;
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
      */
     public BallDemo()
     {
         myCanvas = new Canvas("Ball Demo", 600, 500);
-
+        misBolas = new ArrayList<>();
+        aleatorio = new Random();
     }
 
     /**
@@ -30,41 +34,33 @@ public class BallDemo
 
         myCanvas.setVisible(true);
 
-        misBolas = new BouncingBall[numeroBolas];
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
         
-        Random aleatorio = new Random();
-        // crate and show the balls
-        for(int i = 0; i < numeroBolas; i++){
-            int radio = aleatorio.nextInt(20) + 5;
-            
-            int color1 = aleatorio.nextInt(200);
-            int color2 = aleatorio.nextInt(200);
-            int color3 = aleatorio.nextInt(200);
+        for (int i = 0; i < numeroBolas; i++) {
+            int tamano = aleatorio.nextInt(50) + 5;
+            int color1 = aleatorio.nextInt(220);
+            int color2 = aleatorio.nextInt(220);
+            int color3 = aleatorio.nextInt(220);
             
             int posX = aleatorio.nextInt(300);
-            
-            Color color = new Color(color1,color2,color3);
-            BouncingBall ball = new BouncingBall(posX,50, radio, color, ground, myCanvas);
+            int posY = aleatorio.nextInt(250);
+            Color color = new Color(color1, color2, color3);
+            BouncingBall ball = new BouncingBall(posX, posY, tamano, color, ground, myCanvas);
             ball.draw();
-            misBolas[i]=ball;
+            misBolas.add(ball);
         }
 
         // make them bounce
-
         boolean finished =  false;
         while(!finished) {
             myCanvas.wait(50);           // small delay
-            for(int i = 0; i < numeroBolas; i++){
-                misBolas[i].move();
-                if(misBolas[i].getXPosition() >= 550){
+            for (int i = 0; i < misBolas.size(); i++) {
+                misBolas.get(i).move();
+                if (misBolas.get(i).getXPosition() >= 550) {
                     finished = true;
                 }
             }
-            // stop once ball has travelled a certain distance on x axis
-            
         }
-
     }
 }
