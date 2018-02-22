d5a944220ef5bf87b46812bf49194b3a1a351ce2
nachob99 <nachoarriola99@gmail.com>
2018-02-22 21:03:52 +0100

    Modificado metodo que nos permite crear las bolas mediante parametro

--- BallDemo.java
+++ BallDemo.java
@@ -11,43 +11,48 @@ import java.awt.Color;
 public class BallDemo   
 {
     private Canvas myCanvas;
-
+    private BouncingBall[] misBolas;
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
      */
     public BallDemo()
     {
         myCanvas = new Canvas("Ball Demo", 600, 500);
+
     }
 
     /**
      * Simulate two bouncing balls
      */
-    public void bounce()
+    public void bounce(int numeroBolas)
     {
         int ground = 400;   // position of the ground line
 
         myCanvas.setVisible(true);
-
+        
+        misBolas = new BouncingBall[numeroBolas];
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
 
         // crate and show the balls
-        BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
-        ball.draw();
-        BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
-        ball2.draw();
+        for(int i = 0; i < numeroBolas; i++){
+            BouncingBall ball = new BouncingBall(50 + (10*1),50, 16, Color.BLUE, ground, myCanvas);
+            ball.draw();
+            misBolas[i]=ball;
+        }
 
         // make them bounce
+        /**
         boolean finished =  false;
         while(!finished) {
-            myCanvas.wait(50);           // small delay
-            ball.move();
-            ball2.move();
-            // stop once ball has travelled a certain distance on x axis
-            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
-                finished = true;
-            }
+        myCanvas.wait(50);           // small delay
+        ball.move();
+        ball2.move();
+        // stop once ball has travelled a certain distance on x axis
+        if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
+        finished = true;
         }
+        }
+         */
     }
 }
