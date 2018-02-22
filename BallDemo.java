f20b2b1c6ac725e3ae9a8eb197292e74aac6845f
nachob99 <nachoarriola99@gmail.com>
2018-02-22 21:20:23 +0100

    Modificado metodo que nos permite crear colores y generar la psicionX aleatoria

--- BallDemo.java
+++ BallDemo.java
@@ -1,5 +1,5 @@
 import java.awt.Color;
-
+import java.util.Random;
 /**
  * Class BallDemo - a short demonstration showing animation with the 
  * Canvas class. 
@@ -33,10 +33,20 @@ public class BallDemo
         misBolas = new BouncingBall[numeroBolas];
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
-
+        
+        Random aleatorio = new Random();
         // crate and show the balls
         for(int i = 0; i < numeroBolas; i++){
-            BouncingBall ball = new BouncingBall(50 + (10*1),50, 16, Color.BLUE, ground, myCanvas);
+            int radio = aleatorio.nextInt(20) + 5;
+            
+            int color1 = aleatorio.nextInt(200);
+            int color2 = aleatorio.nextInt(200);
+            int color3 = aleatorio.nextInt(200);
+            
+            int posX = aleatorio.nextInt(300);
+            
+            Color color = new Color(color1,color2,color3);
+            BouncingBall ball = new BouncingBall(posX,50, radio, color, ground, myCanvas);
             ball.draw();
             misBolas[i]=ball;
         }
